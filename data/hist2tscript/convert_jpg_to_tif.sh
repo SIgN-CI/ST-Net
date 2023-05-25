c951ca55fa6abfe8dfd8bdda962eb562abe662b8
AnonymousEliforp <bokleong.c@gmail.com>
2023-05-25 17:36:23 +0800

    Delete files

--- data/hist2tscript/convert_jpg_to_tif.sh
+++
@@ -1,5 +0,0 @@
-for i in ./HE_BC*.jpg;
-do
-    echo ${i}
-    convert ${i} -define tiff:tile-geometry=256x256 ${i%.jpg}.tif
-done
--- zip_figures.sh
+++
@@ -1,7 +0,0 @@
-# patient=BC50027
-
-echo "Enter patient number (in BCxxxxx format):"
-read patient
-
-echo "Zipping figures for patient ${patient}..."
-zip -r ${patient}_figures.zip fig/visualize/${patient}_*.pdf
